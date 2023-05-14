e9b16a5c28ba6e822d652daf71ac1c301ece2bd6
VladH3V <106373773+VladH3V@users.noreply.github.com>
2023-05-14 11:21:07 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,70 @@
+import tkinter
+import customtkinter
+from pytube import YouTube
+
+
+def StartDownload():
+    try:
+        ytlink = link.get()
+        ytObject = YouTube(ytlink, on_progress_callback=on_progress)
+        video = ytObject.streams.get_highest_resolution()
+
+        title.configure(text=ytObject.title, text_color="white")
+        finishLabel.configure(text="")
+        video.download()
+        finishLabel.configure(text="Downloaded!", text_color="green")
+        
+        
+    except:
+        finishLabel.configure(text="Download Error", text_color="red")
+    
+        
+    
+
+def on_progress(stream, chunk, bytes_remaining):
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+    percentage_of_completion = bytes_downloaded / total_size * 100
+    per = str(int(percentage_of_completion))
+    pPercentage.configure(text=per +"%")
+    pPercentage.update()
+    
+
+# System Settings
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+# Our app frame
+app = customtkinter.CTk()
+app.geometry("720x480")
+app.title("Youtube Downloader")
+
+# Adding UI Elements
+title = customtkinter.CTkLabel(app, text="Insert a youtube link")
+title.pack(padx=10, pady=10)
+
+# Link imput
+url_var = tkinter.StringVar()
+link = customtkinter.CTkEntry(app, width=350, height=40)
+link.pack()
+
+#Finished Downloading
+finishLabel = customtkinter.CTkLabel(app, text="")
+finishLabel.pack()
+
+# Progress percentage
+pPercentage = customtkinter.CTkLabel(app, text="0%")
+pPercentage.pack()
+
+progressBar = customtkinter.CTkProgressBar(app, width=400)
+progressBar.set(0)
+progressBar.pack(padx=10, pady=10)
+
+
+# Download Button
+download = customtkinter.CTkButton(app, text="Download", command=StartDownload)
+download.pack(padx=10, pady=10)
+
+
+# Run app
+app.mainloop()
